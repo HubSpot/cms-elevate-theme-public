69056f262c3538d5f2614a4a16d3bee6b20fbc82
root <root@deployment-mainline-5d996bd5b5-mr4zh>
2026-07-29 15:12:10 +0000

    Copying dir src

---
+++ src/theme/elevate/components/modules/MediaAndSummary/index.tsx
@@ -0,0 +1,224 @@
+import { useId, type ReactNode } from 'react';
+import { ModuleMeta } from '../../types/modules.js';
+import { RichText } from '@hubspot/cms-components';
+import { ImageFieldType, LinkFieldType, TextFieldType, BooleanFieldType, RichTextFieldType } from '@hubspot/cms-components/fields';
+import { CardVariantType } from '../../types/fields.js';
+import { CardStyleFieldLibraryType } from '../../fieldLibrary/CardStyle/types.js';
+import { CSSPropertiesMap } from '../../types/components.js';
+import { getLinkFieldHref, getLinkFieldRel, getLinkFieldTarget } from '../../utils/content-fields.js';
+import { getCardVariantClassName } from '../../utils/card-variants.js';
+import cx, { staticWithModule } from '../../utils/classnames.js';
+import { createComponent } from '../../utils/create-component.js';
+import { getDataHSToken } from '../../utils/inline-editing.js';
+import mediaAndSummaryIconSvg from './assets/media-and-summary.svg';
+import styles from './media-and-summary.module.css';
+
+const swm = staticWithModule(styles);
+
+type MediaAndSummaryProps = {
+  moduleName?: string;
+  group_logo: {
+    customerLogo: ImageFieldType['default'];
+  };
+  group_summary: {
+    industryLabel: TextFieldType['default'];
+    industry: TextFieldType['default'];
+    challengeLabel: TextFieldType['default'];
+    challengeRichText: RichTextFieldType['default'];
+    resultsLabel: TextFieldType['default'];
+    resultsRichText: RichTextFieldType['default'];
+    keyProductLabel: TextFieldType['default'];
+    keyProduct: TextFieldType['default'];
+  };
+  group_link: {
+    showWebsiteLink: BooleanFieldType['default'];
+    linkText: TextFieldType['default'];
+    websiteUrl: LinkFieldType['default'];
+  };
+  group_style: CardStyleFieldLibraryType;
+};
+
+const FeatureWrapper = createComponent('div');
+const Feature = createComponent('div');
+const ImageContainer = createComponent('div');
+const FeatureImage = createComponent('img');
+const ContentContainer = createComponent('div');
+const DetailsList = createComponent('div');
+const DetailRow = createComponent('div');
+const LabelContainer = createComponent('div');
+const LabelText = createComponent('p');
+const ValueContainer = createComponent('div');
+const ValueParagraph = createComponent('p');
+const LinkContainer = createComponent('div');
+const LinkAnchor = createComponent('a');
+
+function generateColorCssVars(cardVariantField: string): CSSPropertiesMap {
+  const cardColorsMap = {
+    card_variant_1: {
+      textColor: 'var(--hsElevate--card--variant1__textColor)',
+      backgroundColor: 'var(--hsElevate--card--variant1__backgroundColor)',
+      borderColor: 'var(--hsElevate--card--variant1__borderColor)',
+      borderRadius: 'var(--hsElevate--card--variant1__borderRadius)',
+      borderThickness: 'var(--hsElevate--card--variant1__borderThickness)',
+      linkFontColor: 'var(--hsElevate--card--variant1--link__fontColor)',
+      linkHoverFontColor: 'var(--hsElevate--card--variant1--link__hover--fontColor)',
+    },
+    card_variant_2: {
+      textColor: 'var(--hsElevate--card--variant2__textColor)',
+      backgroundColor: 'var(--hsElevate--card--variant2__backgroundColor)',
+      borderColor: 'var(--hsElevate--card--variant2__borderColor)',
+      borderRadius: 'var(--hsElevate--card--variant2__borderRadius)',
+      borderThickness: 'var(--hsElevate--card--variant2__borderThickness)',
+      linkFontColor: 'var(--hsElevate--card--variant2--link__fontColor)',
+      linkHoverFontColor: 'var(--hsElevate--card--variant2--link__hover--fontColor)',
+    },
+    card_variant_3: {
+      textColor: 'var(--hsElevate--card--variant3__textColor)',
+      backgroundColor: 'var(--hsElevate--card--variant3__backgroundColor)',
+      borderColor: 'var(--hsElevate--card--variant3__borderColor)',
+      borderRadius: 'var(--hsElevate--card--variant3__borderRadius)',
+      borderThickness: 'var(--hsElevate--card--variant3__borderThickness)',
+      linkFontColor: 'var(--hsElevate--card--variant3--link__fontColor)',
+      linkHoverFontColor: 'var(--hsElevate--card--variant3--link__hover--fontColor)',
+    },
+    card_variant_4: {
+      textColor: 'var(--hsElevate--card--variant4__textColor)',
+      backgroundColor: 'var(--hsElevate--card--variant4__backgroundColor)',
+      borderColor: 'var(--hsElevate--card--variant4__borderColor)',
+      borderRadius: 'var(--hsElevate--card--variant4__borderRadius)',
+      borderThickness: 'var(--hsElevate--card--variant4__borderThickness)',
+      linkFontColor: 'var(--hsElevate--card--variant4--link__fontColor)',
+      linkHoverFontColor: 'var(--hsElevate--card--variant4--link__hover--fontColor)',
+    },
+  };
+
+  const cardColors = cardColorsMap[cardVariantField];
+
+  return {
+    '--hsElevate--mediaAndSummary__textColor': cardColors.textColor,
+    '--hsElevate--mediaAndSummary__backgroundColor': cardColors.backgroundColor,
+    '--hsElevate--mediaAndSummary__borderColor': cardColors.borderColor,
+    '--hsElevate--mediaAndSummary__borderRadius': cardColors.borderRadius,
+    '--hsElevate--mediaAndSummary__borderThickness': cardColors.borderThickness,
+    '--hsElevate--mediaAndSummary__linkFontColor': cardColors.linkFontColor,
+    '--hsElevate--mediaAndSummary__linkHoverFontColor': cardColors.linkHoverFontColor,
+    '--hsElevate--mediaAndSummary__accentColor': cardColors.linkFontColor,
+  };
+}
+
+function DetailGroup({ label, children }: { label: string; children: ReactNode }) {
+  const labelId = useId();
+  return (
+    <DetailRow className="hs-elevate-details" role="group" aria-labelledby={labelId}>
+      <LabelContainer className="hs-elevate-details_label-container">
+        <LabelText id={labelId} className="hs-elevate-details__label">
+          {label}
+        </LabelText>
+      </LabelContainer>
+      <ValueContainer className="hs-elevate-details__value-container">{children}</ValueContainer>
+    </DetailRow>
+  );
+}
+
+function FeatureChevron() {
+  return (
+    <svg className="cs-feature__chevron" xmlns="http://www.w3.org/2000/svg" width="16" height="10" viewBox="0 0 16 10" fill="none" aria-hidden="true">
+      <path
+        className="cs-feature__chevron-path"
+        d="M9.79399 4.10043L2.99372 0.190703C2.16891 -0.283516 1.10262 0.178478 0.961074 1.00594C0.940125 1.12841 0.968466 1.2526 1.01359 1.36836L2.42912 5.00004L1.01359 8.63172C0.968466 8.74748 0.940121 8.87169 0.961111 8.99414C1.10095 9.80995 2.14126 10.2741 2.96632 9.82472L9.76659 6.12076C10.6065 5.66331 10.6212 4.57602 9.79399 4.10043Z"
+      />
+    </svg>
+  );
+}
+
+export const Component = (props: MediaAndSummaryProps) => {
+  const {
+    moduleName,
+    group_logo: { customerLogo },
+    group_summary: { industryLabel, industry, challengeLabel, resultsLabel, keyProductLabel, keyProduct },
+    group_link: { showWebsiteLink, linkText, websiteUrl },
+    group_style: { cardStyleVariant },
+  } = props;
+
+  const cardVariantClass = getCardVariantClassName({ cardVariant: cardStyleVariant as CardVariantType, fallbackCardVariant: 'card_variant_2' });
+  const cssVarsMap = generateColorCssVars(cardStyleVariant);
+  const linkHref = getLinkFieldHref(websiteUrl);
+  const showLink = showWebsiteLink && linkHref;
+
+  return (
+    <FeatureWrapper className={cx('cs-feature-wrapper', swm('moduleRoot'))}>
+      <Feature className={cx('cs-feature', cardVariantClass)} style={cssVarsMap}>
+        <ImageContainer className="cs-feature__image-container">
+          {Boolean(customerLogo.src) && (
+            <FeatureImage
+              className={cx('cs-feature__image', swm('featureImage'))}
+              src={customerLogo.src}
+              alt={customerLogo.alt || 'Featured image'}
+              width={customerLogo.width}
+              height={customerLogo.height}
+              loading={customerLogo.loading !== 'disabled' ? customerLogo.loading : 'eager'}
+              data-hs-token={getDataHSToken(moduleName, 'group_logo.customerLogo')}
+            />
+          )}
+        </ImageContainer>
+        <ContentContainer className="cs-feature__content-container">
+          <DetailsList className="cs-feature__details">
+            <DetailGroup label={industryLabel}>
+              <ValueParagraph className="hs-elevate-details__value" data-hs-token={getDataHSToken(moduleName, 'group_summary.industry')}>
+                {industry}
+              </ValueParagraph>
+            </DetailGroup>
+            <DetailGroup label={challengeLabel}>
+              <RichText
+                fieldPath="group_summary.challengeRichText"
+                className={cx('hs-elevate-details__value', swm('detailRichText'))}
+                data-hs-token={getDataHSToken(moduleName, 'group_summary.challengeRichText')}
+              />
+            </DetailGroup>
+            <DetailGroup label={resultsLabel}>
+              <RichText
+                fieldPath="group_summary.resultsRichText"
+                className={cx('hs-elevate-details__value', swm('detailRichText'))}
+                data-hs-token={getDataHSToken(moduleName, 'group_summary.resultsRichText')}
+              />
+            </DetailGroup>
+            <DetailGroup label={keyProductLabel}>
+              <ValueParagraph className="hs-elevate-details__value" data-hs-token={getDataHSToken(moduleName, 'group_summary.keyProduct')}>
+                {keyProduct}
+              </ValueParagraph>
+            </DetailGroup>
+          </DetailsList>
+          {showLink && (
+            <LinkContainer className="cs-feature__link-container">
+              <LinkAnchor
+                className="cs-feature__link"
+                href={linkHref}
+                rel={getLinkFieldRel(websiteUrl)}
+                target={getLinkFieldTarget(websiteUrl)}
+                data-hs-token={getDataHSToken(moduleName, 'group_link.websiteUrl')}
+              >
+                <span data-hs-token={getDataHSToken(moduleName, 'group_link.linkText')}>{linkText}</span>
+                <FeatureChevron />
+              </LinkAnchor>
+            </LinkContainer>
+          )}
+        </ContentContainer>
+      </Feature>
+    </FeatureWrapper>
+  );
+};
+
+export { fields } from './fields.js';
+
+export const meta: ModuleMeta = {
+  label: 'Media and summary',
+  content_types: ['BLOG_POST'],
+  icon: mediaAndSummaryIconSvg,
+  categories: ['body_content'],
+};
+
+export const defaultModuleConfig = {
+  moduleName: 'elevate/components/modules/media_and_summary',
+  version: 0,
+  themeModule: true,
+};
